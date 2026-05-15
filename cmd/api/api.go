8c776914c8f6d16b5fe6bda1c19d26dc4aed2a40
Jehad Mohamed <jjehad100@gmail.com>
2026-05-15 18:38:11 +0200

    workspace handler : create workspace / api :protected router , workspace

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/Mikhail-tal63/viltrum_empier/internal/auth"
+	"github.com/Mikhail-tal63/viltrum_empier/internal/workspace"
 	"github.com/Mikhail-tal63/viltrum_empier/middleware"
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -30,6 +31,18 @@ func (s *APIServer) Run() error {
 	userService := auth.NewAuthService(userRepo)
 	userHandler := auth.NewAuthHandler(userService)
 	userHandler.AuthRouter(apiRouter.PathPrefix("/auth").Subrouter())
+
+	/*protected router*************************************************/
+
+	protectedRouter := apiRouter.PathPrefix("").Subrouter()
+	protectedRouter.Use(middleware.AuthMiddleware)
+
+	/*workspace*****************************/
+
+	workspacerepo := workspace.NewWorkspaceRepo(s.db)
+	workspaceService := workspace.NewWorkspaceService(workspacerepo)
+	workspaceHandler := workspace.NewWorkspaceHandler(workspaceService)
+	workspaceHandler.WorkspaceRouter(protectedRouter)
 
 	log.Println("listening on ", s.addr)
 
---
+++ internal/workspace/workspace_handler.go
@@ -0,0 +1,46 @@
+package workspace
+
+import (
+	"net/http"
+
+	"github.com/Mikhail-tal63/viltrum_empier/middleware"
+	"github.com/Mikhail-tal63/viltrum_empier/utils/json"
+	"github.com/gorilla/mux"
+)
+
+type WorkspaceHandler struct {
+	WorkspaceService *WorkspaceService
+}
+
+func NewWorkspaceHandler(WorkspaceService *WorkspaceService) *WorkspaceHandler {
+	return &WorkspaceHandler{
+		WorkspaceService: WorkspaceService,
+	}
+
+}
+
+func (h *WorkspaceHandler) WorkspaceRouter(router *mux.Router) {
+	router.HandleFunc("/workspaces", h.CreateWorkspace).Methods("POST")
+}
+
+func (h *WorkspaceHandler) CreateWorkspace(w http.ResponseWriter, r *http.Request) {
+	var payload WorkspacePayload
+	if err := json.ParseJSON(r, &payload); err != nil {
+		json.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+	userID, err := middleware.GetUserID(r.Context())
+	if err != nil {
+		json.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+	workspace, err := h.WorkspaceService.CreateWorkspace(r.Context(), &payload, userID)
+	if err != nil {
+		json.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+	if err := json.WriteJSON(w, http.StatusCreated, workspace); err != nil {
+		json.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+}
