bbdb13ebc196546f6d55991ecce46cc6618a275a
Jehad Mohamed <jjehad100@gmail.com>
2026-05-15 15:49:13 +0200

    api and cors

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,1 +1,38 @@
 package api
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Mikhail-tal63/viltrum_empier/internal/auth"
+	"github.com/Mikhail-tal63/viltrum_empier/middleware"
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type APIServer struct {
+	addr string
+	db   *mongo.Database
+}
+
+func NewAPIServer(addr string, db *mongo.Database) *APIServer {
+	return &APIServer{
+		addr: addr,
+		db:   db,
+	}
+}
+
+func (s *APIServer) Run() error {
+	router := mux.NewRouter()
+	apiRouter := router.PathPrefix("/api/v1").Subrouter()
+
+	userRepo := auth.NewAuthRepository(s.db)
+	userService := auth.NewAuthService(userRepo)
+	userHandler := auth.NewAuthHandler(userService)
+	userHandler.AuthRouter(apiRouter)
+
+	log.Println("listening on ", s.addr)
+
+	return http.ListenAndServe(s.addr, middleware.CORS(router))
+
+}
