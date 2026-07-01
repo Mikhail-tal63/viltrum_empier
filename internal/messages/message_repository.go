88708a03f40eec751b64c1a8e12286bb4b36e952
Jehad Mohamed <jjehad100@gmail.com>
2026-07-01 10:05:53 +0200

    create and delete message : repository

--- internal/messages/message_repository.go
+++ internal/messages/message_repository.go
@@ -1,6 +1,12 @@
 package messages
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type Messagerepo struct {
 	msgRepo   *mongo.Collection
@@ -12,4 +18,21 @@ func NewMessageRepository(db *mongo.Database) *Messagerepo {
 		msgRepo:   db.Collection("message"),
 		groupRepo: db.Collection("group_chat"),
 	}
+}
+
+func (r *Messagerepo) CreateGroupChat(ctx context.Context, Group *GroupChat) (error, *GroupChat) {
+	_, err := r.groupRepo.InsertOne(ctx, Group)
+	if err != nil {
+		return err, nil
+	}
+	return nil, Group
+}
+func (r *Messagerepo) DeleteGroupChat(ctx context.Context, gID primitive.ObjectID) error {
+	filter := bson.M{"_id": gID}
+
+	_, err := r.groupRepo.DeleteOne(ctx, filter)
+	if err != nil {
+		return err
+	}
+	return nil
 }
