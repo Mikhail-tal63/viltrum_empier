b4a1f0375eac7ee861f51382db0a55820fc9d153
Jehad Mohamed <jjehad100@gmail.com>
2026-07-04 11:08:07 +0200

    feat: add broadcast message deleted method to message :message_broadcast

--- internal/messages/message_broadcast.go
+++ internal/messages/message_broadcast.go
@@ -9,7 +9,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-func (s *MessageService) BroadcastCreateGroupChat(ctx context.Context, workspaceID, userID primitive.ObjectID, GC *GroupChat) {
+func (s *MessageService) BroadcastCreateGroupChat(ctx context.Context, userID, workspaceID primitive.ObjectID, GC *GroupChat) {
 
 	payload, err := websocket.MarshalEvent(websocket.EventGroupCreated, map[string]any{
 		"group_chat": GC,
@@ -34,4 +34,14 @@ func (s *MessageService) BroadcastMessageCreated(ctx context.Context, userID, wo
 	}
 	s.messageHup.BroadcastToWorkspace(workspaceID.Hex(), payload)
 
+}
+func (s *MessageService) BroadcastMessageDeleted(ctx context.Context, userID, workspaceID primitive.ObjectID) {
+	payload, err := websocket.MarshalEvent(websocket.EventMessageDeleted, map[string]any{
+		"by": userID.Hex(),
+	})
+	if err != nil {
+		log.Printf("ws: marshal failed: %v", err)
+		return
+	}
+	s.messageHup.BroadcastToWorkspace(workspaceID.Hex(), payload)
 }
