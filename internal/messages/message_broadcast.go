919f8136dac3b051f0408ec6e8a0dafe04a4fa39
Jehad Mohamed <jjehad100@gmail.com>
2026-07-04 11:00:44 +0200

    feat: add broadcast message created method to message :message_broadcast

--- internal/messages/message_broadcast.go
+++ internal/messages/message_broadcast.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/Mikhail-tal63/viltrum_empier/internal/websocket"
+
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -13,6 +14,19 @@ func (s *MessageService) BroadcastCreateGroupChat(ctx context.Context, workspace
 	payload, err := websocket.MarshalEvent(websocket.EventGroupCreated, map[string]any{
 		"group_chat": GC,
 		"by":         userID.Hex(),
+	})
+	if err != nil {
+		log.Printf("ws: marshal failed: %v", err)
+		return
+	}
+	s.messageHup.BroadcastToWorkspace(workspaceID.Hex(), payload)
+
+}
+
+func (s *MessageService) BroadcastMessageCreated(ctx context.Context, userID, workspaceID primitive.ObjectID, message *Message) {
+	payload, err := websocket.MarshalEvent(websocket.EventMessageCreated, map[string]any{
+		"message": message,
+		"by":      userID.Hex(),
 	})
 	if err != nil {
 		log.Printf("ws: marshal failed: %v", err)
